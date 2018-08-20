f869a02c89e4835c6604cdd11e39c49c386f4723
Ryan Mullowney <ryan@wireless-10-145-49-250.public.utexas.edu>
2018-08-20 11:56:35 -0500

    Changes font to white.  Makes all font fize h2 on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export default class Home extends Component {
   render() {
     return (
       <div className="container text-center mt-5">
-        <h2 className="font-weight-light">Hey there!  I'm Ryan.</h2>
+        <h3 className="font-weight-light">Hey there!  I'm Ryan.</h3>
 
         <h3 className="font-weight-light mt-4">I'm a full-stack web developer.</h3>
 
