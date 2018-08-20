6e1372f4b19cab0b3820b841137aa2febd29e551
Ryan Mullowney <ryan@Ryans-MacBook-Pro.local>
2018-08-19 23:50:59 -0500

    Updates CSS on contact page.  Adds alternate gradient color in App.css comments

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,15 +8,15 @@ export default class Contact extends Component {
         <div className="row justify-content-center mt-5">
           {/* Why is the <a> tag making it so the icon and text aren't in the same row anymore?  Made it so it encompasses the entire div and it works around it for now */}
           <a className="mx-5" href="https://github.com/rwmullowney/" target="_blank" style={{ textDecoration: "none" }}>
-              <img className="mx-5" style={{ maxHeight: "128px", maxWidth: "128px", width: "100%", height: "100%"}} src="./images/github.svg" />
+              <img className="mx-5" src="./images/github.svg" />
               {/* <h3 className="font-weight-light">Github</h3> */}
           </a>
           <a className="mx-5" href="https://www.linkedin.com/in/ryan-mullowney-a9309443/" target="_blank" style={{ textDecoration: "none" }}>
-              <img className="mx-5" style={{ maxHeight: "128px", maxWidth: "128px", width: "100%", height: "100%"}} src="./images/linkedin.svg" />
+              <img className="mx-5" src="./images/linkedin.svg" />
               {/* <h3 className="font-weight-light">LinkedIn</h3> */}
           </a >
           <a className="mx-5">
-            <img className="mx-5" style={{ maxHeight: "128px", maxWidth: "128px", width: "100%", height: "100%"}} src="./images/email.svg" />
+            <img className="mx-5" src="./images/email.svg" />
             {/* <h3 className="font-weight-light">Email</h3> */}
             </a>
         </div>
